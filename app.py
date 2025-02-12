2bca31eb3a113f3159e366eb0d1cae55a35d2cdf
Abdullahi <40540833@live.napier.ac.uk>
2025-02-12 13:33:20 +0000

    created basic style and base page

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:1234567890@localhost/health_risk_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db= SQLAlchemy(app)
