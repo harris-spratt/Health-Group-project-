080bf53bae3311dc970a32c1be0176058cb9535b
Abdirahman <40628520@live.napier.ac.uk>
2025-02-22 14:33:20 +0000

    created test page with simple text return of three tests

--- main.py
+++ main.py
@@ -4,11 +4,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")  # Changed from base.html to home.html
+    return render_template("home.html")
+
+@app.route("/tests")
+def tests():
+    return render_template("tests.html")
+
+
+@app.route("/h2fpef")
+def h2fpef():
+    return "H2FPEF Test Page"
+
+@app.route("/qrisk3")
+def qrisk3():
+    return "QRISK3 Test Page"
+
+@app.route("/who_cv")
+def who_cv():
+    return "WHO CV Risk Test Page"
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
 
 
